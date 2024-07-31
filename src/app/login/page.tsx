e6865208f02540295516c73a7077e8f9e3bd5a8b
Dennar David <dennardavid@gmail.com>
2024-07-31 17:38:15 +0100

    Login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,47 @@
+import Link from "next/link";
+
 function Login() {
-  return;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="w-[70%]">
+        <h1 className="text-left mb-2 font-bold text-lg">Login</h1>
+        <form className=" flex flex-col gap-3" >
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+            
+          />
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+            
+          />
+
+          <span className="text-red-500">{}</span>
+          <button
+            type="submit"
+            className="bg-zinc-400/40 text-white p-2 rounded-lg"
+          >
+            Login
+          </button>
+        </form>
+        <div className="text-right mt-3">
+          <p>
+            Don&apos;t have an account?{" "}
+            <Link href="/signup" className="text-blue-500">
+              Sign Up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
