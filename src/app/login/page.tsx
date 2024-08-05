c45ba53bbfe74ab8691155408976f78ed5c9a06e
Dennar David <dennardavid@gmail.com>
2024-08-05 23:32:39 +0100

    login UI

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -22,35 +22,38 @@ function Login() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <div className="w-[70%]">
-        <h1 className="text-left mb-2 font-bold text-lg">Login</h1>
-        <form className=" flex flex-col gap-3" onSubmit={handleSubmit}>
-          <label htmlFor="email">Email:</label>
-          <input type="email" name="email" id="email" placeholder="Email" />
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Password"
-          />
+    <section className="flex">
+      <div className="w-[45%] bg-cyan"></div>
+      <div className="flex flex-col items-center w-[55%] justify-center h-screen">
+        <div className="w-[60%]">
+          <h1 className="text-left mb-2 font-bold text-lg">Login</h1>
+          <form className=" flex flex-col gap-3" onSubmit={handleSubmit}>
+            <label htmlFor="email">Email:</label>
+            <input type="email" name="email" id="email" placeholder="Email" />
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+            />
 
-          {error && <span className="text-red-500">{error}</span>}
-          <button type="submit" className="p-2 rounded-lg" formAction={login}>
-            Login
-          </button>
-        </form>
-        <div className="text-right mt-3">
-          <p>
-            Don&apos;t have an account?{" "}
-            <Link href="/signup" className="text-blue-500">
-              Sign Up
-            </Link>
-          </p>
+            {error && <span className="text-red-500">{error}</span>}
+            <button type="submit" className="p-2 rounded-lg" formAction={login}>
+              Login
+            </button>
+          </form>
+          <div className="text-right mt-3">
+            <p>
+              Don&apos;t have an account?{" "}
+              <Link href="/signup" className="text-blue-500">
+                Sign Up
+              </Link>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
 
