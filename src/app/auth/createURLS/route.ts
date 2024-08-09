46fdbe04dfe1df7501cbe6b87ebf3e6ddd7c2ae2
Dennardavid <dennardavid@gmail.com>
2024-08-09 18:04:40 +0100

    Posting QR code to database

--- src/app/auth/createURLS/route.ts
+++ src/app/auth/createURLS/route.ts
@@ -1,13 +1,17 @@
 import { createClient } from "../../../utils/supbase/server";
 import { NextResponse } from "next/server";
 
-export async function POST({ original_url, custom_url, user_id, tittle }, qr_code: string) {
+export async function POST(
+  { original_url, custom_url, user_id, tittle },
+  qr_code: string
+) {
   const supabase = createClient();
 
-  const short_url = Math.random().toString(36).substring(2,7);
-  const filename =`qr_${short_url}`;
-  const { error: storageError } = await supabase.storage.from('QR_code').upload('file_path', filename)
-
+  const short_url = Math.random().toString(36).substring(2, 7);
+  const filename = `qr_${short_url}`;
+  const { error: storageError } = await supabase.storage
+    .from("QR_code")
+    .upload("file_path", filename);
 
   if (storageError) {
     console.log("error uploading QR code URLs");
@@ -15,7 +19,9 @@ export async function POST({ original_url, custom_url, user_id, tittle }, qr_cod
   }
   const qr = `${supabaseUrl}/storage/v1/object/public/QR_code/${filename}`;
 
-  const { data,error } = await supabase.from('URLS').upload('file_path', filename)
+  const { data, error } = await supabase
+    .from("URLS")
+    .insert([{ original_url, custom_url, user_id, tittle, qr_code: qr }]);
 
   return NextResponse.json(data, { status: 200 });
 }
